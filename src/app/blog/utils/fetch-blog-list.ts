5a396e04089841176350d39570be46409adcb34c
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-02-27 14:49:55 -0500

    chore: formatting

--- src/app/blog/utils/fetch-blog-list.ts
+++ src/app/blog/utils/fetch-blog-list.ts
@@ -1,29 +1,29 @@
 import client from "@/../tina/__generated__/client";
 
 export async function fetchBlogList() {
-	/**
-	 * TODO: implement pagination. Since 2020, there have been
-	 * 30 blog entries. We can probably wait to implement pagination
-	 * until we have more than 50 blog entries.
-	 */
-	const blogListData = await client.queries.blogConnection({
-		sort: "date",
-		last: 50,
-	});
-	if (!blogListData?.data?.blogConnection?.edges) {
-		throw new Error("Failed to fetch blog list data");
-	}
+  /**
+   * TODO: implement pagination. Since 2020, there have been
+   * 30 blog entries. We can probably wait to implement pagination
+   * until we have more than 50 blog entries.
+   */
+  const blogListData = await client.queries.blogConnection({
+    sort: "date",
+    last: 50,
+  });
+  if (!blogListData?.data?.blogConnection?.edges) {
+    throw new Error("Failed to fetch blog list data");
+  }
 
-	const blogEntries = [];
-	for (const blogEntry of blogListData.data.blogConnection.edges) {
-		const blogNode = blogEntry?.node;
-		if (!blogNode) {
-			throw new Error("Failed to grab node off blog entry");
-		}
-		const { title, category } = blogNode;
-		const { filename } = blogNode._sys;
-		blogEntries.push({ title, filename, category });
-	}
+  const blogEntries = [];
+  for (const blogEntry of blogListData.data.blogConnection.edges) {
+    const blogNode = blogEntry?.node;
+    if (!blogNode) {
+      throw new Error("Failed to grab node off blog entry");
+    }
+    const { title, category } = blogNode;
+    const { filename } = blogNode._sys;
+    blogEntries.push({ title, filename, category });
+  }
 
-	return blogEntries;
+  return blogEntries;
 }
