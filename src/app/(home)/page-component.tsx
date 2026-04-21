e98cd1886d0be18a6c218b00a08fa5fa7896741c
p3london <p3london@proton.me>
2026-04-21 19:05:58 -0400

    feat: implement home page message bars (#6)
    Co-authored-by: Zach Shilton <4624598+zchsh@users.noreply.github.com>

--- src/app/(home)/page-component.tsx
+++ src/app/(home)/page-component.tsx
@@ -1,5 +1,5 @@
 // Third-party
-import { TinaMarkdown } from "tinacms/dist/rich-text";
+import { TinaMarkdown, TinaMarkdownContent } from "tinacms/dist/rich-text";
 // Components
 import PrimaryButtonLink from "@/components/primary-button-link";
 import Spacer from "@/components/spacer";
@@ -9,58 +9,119 @@ import ThreeFeatures from "./components/three-features";
 import VideoHero from "./components/video-hero";
 // Styles
 import s from "./home-page.module.css";
+import MessageBar from "./components/message-bar";
 
 export default function Home({ data }: $TSFixMe) {
-	const {
-		heading,
-		infoSections,
-		callsToAction,
-		sponsorsHeading,
-		sponsorsText,
-		sponsors,
-	} = data.homepage;
+  const {
+    heading,
+    infoSections,
+    callsToAction,
+    sponsorsHeading,
+    sponsorsText,
+    sponsors,
+    messageBars,
+  } = data.homepage;
 
-	return (
-		<main className={s.root}>
-			{/* <pre>
+  // const messageBars = [
+  //   {
+  //     emoji: "📅",
+  //     title: "Join us for some event!",
+  //     body: {
+  //       type: "root",
+  //       children: [
+  //         {
+  //           type: "p",
+  //           children: [
+  //             {
+  //               type: "text",
+  //               text: "On this date, blah blah blah, pollinator species are responsible for 1 out of every 3 bites of food we eat. Pollination is a crucial process in the reproductive system of plants.",
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //     links: [
+  //       { linkText: "Link one", linkUrl: "https://www.example.com" },
+  //       { linkText: "Link two", linkUrl: "https://www.example.com" },
+  //     ],
+  //   },
+  //   {
+  //     emoji: "🪴",
+  //     title: "Plant sale!",
+  //     body: {
+  //       type: "root",
+  //       children: [
+  //         {
+  //           type: "p",
+  //           children: [
+  //             {
+  //               type: "text",
+  //               text: "On this date, blah blah blah, pollinator species are responsible for 1 out of every 3 bites of food we eat. Pollination is a crucial process in the reproductive system of plants.",
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //     links: [
+  //       { linkText: "Link one", linkUrl: "https://www.example.com" },
+  //       { linkText: "Link two", linkUrl: "https://www.example.com" },
+  //     ],
+  //   },
+  // ];
+
+  return (
+    <main className={s.root}>
+      {/* <pre>
 					<code>{JSON.stringify(data.homepage, null, 2)}</code>
 				</pre> */}
-			<VideoHero
-				videoSrc={"/pollinator-pathways-video.mp4"}
-				heading={heading}
-			/>
-			<div className={s.infoSections}>
-				{infoSections.map((section: $TSFixMe, index: number) => {
-					const flipOrder = index % 2 !== 0;
-					return (
-						<TextImageSplit
-							key={index}
-							flipOrder={flipOrder}
-							heading={section.title}
-							image={section.image}
-						>
-							<div className={s.textImageBody}>
-								<TinaMarkdown content={section.body} />
-							</div>
-							{section.linkText && section.linkUrl ? (
-								<PrimaryButtonLink
-									className={s.textImageLink}
-									href={section.linkUrl}
-								>
-									{section.linkText}
-								</PrimaryButtonLink>
-							) : null}
-						</TextImageSplit>
-					);
-				})}
-			</div>
-			<Spacer h="2rem" />
-			<ThreeFeatures items={callsToAction} />
-			<SponsorsSection
-				heading={sponsorsHeading}
-				text={sponsorsText}
-				sponsors={sponsors}
-			/>
-		</main>
-	);
+      <VideoHero
+        videoSrc={"/pollinator-pathways-video.mp4"}
+        heading={heading}
+      />
+      {Array.isArray(messageBars) && messageBars.length > 0
+        ? messageBars.map((bar, index) => (
+            <MessageBar
+              key={index}
+              emoji={bar.emoji}
+              title={bar.title}
+              body={bar.body as unknown as TinaMarkdownContent}
+              links={bar.links}
+            />
+          ))
+        : null}
+
+      <div className={s.infoSections}>
+        {infoSections.map((section: $TSFixMe, index: number) => {
+          const flipOrder = index % 2 !== 0;
+          return (
+            <TextImageSplit
+              key={index}
+              flipOrder={flipOrder}
+              heading={section.title}
+              image={section.image}
+            >
+              <div className={s.textImageBody}>
+                <TinaMarkdown content={section.body} />
+              </div>
+              {section.linkText && section.linkUrl ? (
+                <PrimaryButtonLink
+                  className={s.textImageLink}
+                  href={section.linkUrl}
+                >
+                  {section.linkText}
+                </PrimaryButtonLink>
+              ) : null}
+            </TextImageSplit>
+          );
+        })}
+      </div>
+      <Spacer h="2rem" />
+      <ThreeFeatures items={callsToAction} />
+      <SponsorsSection
+        heading={sponsorsHeading}
+        text={sponsorsText}
+        sponsors={sponsors}
+      />
+    </main>
+  );
 }
--- src/app/(home)/tina-config.ts
+++ src/app/(home)/tina-config.ts
@@ -12,6 +12,52 @@ export async function tinaQuery() {
 }
 
 /**
+
+{
+  emoji: "📅",
+  title: "Join us for some event!",
+  body: {
+    type: "root",
+    children: [
+      {
+        type: "p",
+        children: [
+          {
+            type: "text",
+            text: "On this date, blah blah blah, pollinator species are responsible for 1 out of every 3 bites of food we eat. Pollination is a crucial process in the reproductive system of plants.",
+          },
+        ],
+      },
+    ],
+  },
+  links: [
+    { linkText: "Link one", linkUrl: "https://www.example.com" },
+    { linkText: "Link two", linkUrl: "https://www.example.com" },
+  ],
+},
+
+ */
+
+const messageBarFields = [
+  {
+    type: "string",
+    label: "Emoji",
+    name: "emoji",
+    required: true,
+  },
+  {
+    type: "string",
+    label: "Title",
+    name: "title",
+    required: true,
+  },
+  {
+    type: "rich-text",
+    label: "Body",
+    name: "body",
+  },
+];
+/**
  * Define the shape of this data in the CMS.
  * This is the data that ultimately gets saved to a JSON file,
  * and returned by the `tinaQuery` function above.
@@ -33,6 +79,54 @@ export const homePageTinaConfig: Collection = {
       type: "string",
       label: "Heading",
       name: "heading",
+    },
+    {
+      list: true,
+      type: "object",
+      label: "Message Bars",
+      name: "messageBars",
+      ui: { itemProps: (item) => ({ label: item?.title }) },
+      fields: [
+        {
+          type: "string",
+          label: "Emoji",
+          name: "emoji",
+          required: true,
+        },
+        {
+          type: "string",
+          label: "Title",
+          name: "title",
+          required: true,
+        },
+        {
+          type: "rich-text",
+          name: "body",
+          label: "Body",
+          isBody: true,
+        },
+        {
+          list: true,
+          type: "object",
+          label: "Links",
+          name: "links",
+          ui: { itemProps: (item) => ({ label: item?.linkText }) },
+          fields: [
+            {
+              type: "string",
+              label: "Link Text",
+              name: "linkText",
+              required: true,
+            },
+            {
+              type: "string",
+              label: "Link URL",
+              name: "linkUrl",
+              required: true,
+            },
+          ],
+        },
+      ],
     },
     {
       list: true,
