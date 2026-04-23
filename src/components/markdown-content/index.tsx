4706867bb2a41205839b04d737dc4daeeb60dfe5
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-04-23 09:58:29 -0400

    feat: add PDF file support to blog entries

--- src/components/markdown-content/index.tsx
+++ src/components/markdown-content/index.tsx
@@ -1,9 +1,43 @@
 import clsx from "clsx";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
+// Components
+import { ButtonLink } from "../primary-button-link";
 // Styles
 import s from "./markdown-content.module.css";
 // Types
 import type { TinaMarkdownContent } from "tinacms/dist/rich-text";
+
+function mdA(props: $TSFixMe) {
+  const { type, title, url, _content_source, children, ...restProps } = props;
+  const isPdfLink = url?.endsWith(".pdf");
+  if (isPdfLink) {
+    const icon = "/uploads/Site-wide/icon-file.png";
+    return (
+      <ButtonLink
+        {...restProps}
+        styleVariant="primary"
+        href={url}
+        download={title}
+        target="_blank"
+      >
+        <span className={s.mdA_iconAndText}>
+          <img className={s.mdA_buttonIcon} src={icon} alt="" />
+          {children}
+        </span>
+      </ButtonLink>
+    );
+  } else {
+    return (
+      <a {...restProps} href={url} className={s.mdA}>
+        {children}
+      </a>
+    );
+  }
+}
+
+const markdownComponentMapping = {
+  a: mdA,
+};
 
 export function MarkdownContent({
   content,
@@ -14,7 +48,7 @@ export function MarkdownContent({
 }) {
   return (
     <div className={clsx(s.root, className)}>
-      <TinaMarkdown content={content} />
+      <TinaMarkdown content={content} components={markdownComponentMapping} />
     </div>
   );
 }
